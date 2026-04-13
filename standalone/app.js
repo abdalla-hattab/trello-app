a98aa2e81a5b32f7e041253c009c3b0869f6b085
abdalla-hattab <abdla.hthth@gmail.com>
2026-04-13 21:15:25 +0300

    feat(ui): display list checkmark button unconditionally on the right side of the add tasks button

--- standalone/app.js
+++ standalone/app.js
@@ -7553,13 +7553,10 @@ function renderKanbanApp(activeBoard) {
             updateCheckBtnVisuals();
         };
 
-        if (activeBoard.showListCheck && activeBoard.showListCheck[list.id]) {
-            footerRow.appendChild(listCheckBtn);
-        }
-
         addBtn.style.flexGrow = '1';
         addBtn.style.margin = '0'; 
         footerRow.appendChild(addBtn);
+        footerRow.appendChild(listCheckBtn);
 
         listContainer.appendChild(footerRow);
 
